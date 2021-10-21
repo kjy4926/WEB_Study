2b0699756e98205b2d1aad2af1a2a7ea1ac314c5
jyKim <kjy4926@gmail.com>
2021-10-22 02:56:18 +0900

    React props study

--- react-app/my-react-app/src/App.js
+++ react-app/my-react-app/src/App.js
@@ -1,51 +1,18 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content'
+import Subject from './components/Subject'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject/>
+        <Subject title="WEB" sub="World Wide Web "></Subject>
+        <Subject title="React" sub="For UI"></Subject>
         <TOC/>
-        <Content/>
+        <Content title="HTML" desc="Hyper Text Markup Language"></Content>
       </div>
-    );
-  }
-}
-
-class Subject extends Component {
-  render() {
-    return(
-      <header>
-          <h1>WEB</h1>
-          World Wide Web!
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li><a href="./HTML.html">HTML</a></li>
-          <li><a href="./CSS.html">CSS</a></li>
-          <li><a href="./JS.html">JavaScript</a></li>
-          <li><a href="./PHP.html">PHP</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render(){
-    return(
-      <article>
-        <h1>HTML</h1>
-        Hyper Text Markup Language
-      </article>
     );
   }
 }
---
+++ react-app/my-react-app/src/components/TOC.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render(){
+      return(
+        <nav>
+          <ul>
+            <li><a href="./HTML.html">HTML</a></li>
+            <li><a href="./CSS.html">CSS</a></li>
+            <li><a href="./JS.html">JavaScript</a></li>
+            <li><a href="./PHP.html">PHP</a></li>
+          </ul>
+        </nav>
+      );
+    }
+  }
+  export default TOC;
