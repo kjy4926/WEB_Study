3a20bb41b62306ad980e0900a1752d12d17ebf54
jyKim <kjy4926@gmail.com>
2021-10-31 03:54:47 +0900

    react event study

--- react-app/my-react-app/src/App.js
+++ react-app/my-react-app/src/App.js
@@ -8,19 +8,29 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subject_web:{title:"WEB", sub:"World Wide Web"},
-      subject_react:{title:"React", sub:"For UI"},
+      node:'welcome',
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      subject_web:{title:'WEB', sub:'World Wide Web'},
+      subject_react:{title:'React', sub:'For UI'},
       contents:[
-        {id:1, link:"/docs/HTML.html", title:"HTML", desc:"HTML is Hyper Text Language"},
-        {id:2, link:"/docs/CSS.html", title:"CSS", desc:"CSS is design"},
-        {id:3, link:"/docs/JS.html", title:"JavaScript", desc:"JavaScript is for interactive"},
-        {id:4, link:"/docs/PHP.html", title:"PHP", desc:"PHP is Hypertext Preprocessor"}
+        {id:1, link:'/docs/HTML.html', title:'HTML', desc:'HTML is Hyper Text Language'},
+        {id:2, link:'/docs/CSS.html', title:'CSS', desc:'CSS is design'},
+        {id:3, link:'/docs/JS.html', title:'JavaScript', desc:'JavaScript is for interactive'},
+        {id:4, link:'/docs/PHP.html', title:'PHP', desc:'PHP is Hypertext Preprocessor'}
       ]
     }
   }
-  render() {
+  render(){
+    var _title, _desc = null;
+    if(this.state.node === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.node === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
-      <div className="App">
+      <div className='App'>
         <Subject 
           title={this.state.subject_web.title} 
           sub={this.state.subject_web.sub}>
@@ -31,8 +41,8 @@ class App extends Component {
         </Subject>
         <TOC data={this.state.contents}></TOC>
         <Content 
-          title={this.state.contents[0].title} 
-          desc={this.state.contents[0].desc}>
+          title={_title} 
+          desc={_desc}>
         </Content>
       </div>
     );
